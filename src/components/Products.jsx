30909f2d3e805458eb685c09cb079792332e9e18
Sachhyam 28 <sachhyam28@gmail.com>
2025-04-29 16:26:01 +0545

    minor change

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,7 +12,7 @@ function Products() {
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
           {products.map((product, index) => (
             <div key={index} className="bg-white p-4 rounded-lg shadow-sm hover:shadow-md">
-                <img src={product.image} alt={product.name} className='w-full h-50 object-cover mb-2 rounded'/>
+                <img src={product.image} alt={product.name} className='w-full h-auto max-h-48 object-contain mb-2 rounded'/>
                 <h2 className='font-bold '>{product.name}</h2>
                 <div className='font-semibold'>Rs. {product.price}</div>
                 <div className='text-center'>
