f51659c5a9ba32cb4de50f87a66a477880d2beec
Sachhyam 28 <sachhyam28@gmail.com>
2025-05-05 16:32:14 +0545

    no change

--- react/src/contexts/CartContext.jsx
+++ react/src/contexts/CartContext.jsx
@@ -1,41 +1,53 @@
-import React, {createContext, useContext, useState} from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 
 const CartContext = createContext()
 
-export const CartProvider = ({children}) => {
-  const [cartItems, setCartItems] = useState ([])
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState(() => {
+    const storedCart = localStorage.getItem('cart')
+    return storedCart ? JSON.parse(storedCart) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cartItems))
+  }, [cartItems])
+
   const addToCart = (product) => {
-      setCartItems((prev) => {
-        const exists =prev.find((item) => item.id === product.id)
-        if (exists) {
-          return prev.map((item) => 
-          item.id === product.id ? {...item, quantity: item.quantity +1}:item
-          )
-        }
-        return [...prev, {...product, quantity:1}]
-      })  
+    setCartItems((prev) => {
+      const exists = prev.find((item) => item.id === product.id)
+      if (exists) {
+        return prev.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      }
+      return [...prev, { ...product, quantity: 1 }]
+    })
   }
+
   const removeFromCart = (id) => {
-    setCartItems ((prev) => prev.filter((item) => item.id !== id))
+    setCartItems((prev) => prev.filter((item) => item.id !== id))
   }
+
   const updateQuantity = (id, quantity) => {
-    setCartItems((prev) => 
-      prev.map((item) => 
-        item.id === id ? {...item, quantity: Number(quantity)}: item
+    setCartItems((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, quantity: Number(quantity) } : item
       )
     )
   }
 
   const clearCart = () => {
     setCartItems([])
+    localStorage.removeItem('cart')
   }
 
   return (
-    <CartContext.Provider value={{cartItems, addToCart, removeFromCart, updateQuantity, clearCart}}>
-        {children}
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, updateQuantity, clearCart }}
+    >
+      {children}
     </CartContext.Provider>
   )
 }
 
 export const useCart = () => useContext(CartContext)
-
